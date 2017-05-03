c64760e43b6719cec8e2dd7ef5044422d981962a
y-xue <yexue14@gmail.com>
2017-05-03 17:29:38 -0500

    first commit

---
+++ L2/src/upload_files.sh
@@ -0,0 +1,4 @@
+sshpass -p "Xy=1993top1" scp compiler.cpp yxe836@hanlon.wot.eecs.northwestern.edu:compiler/322_framework/L2/src
+sshpass -p "Xy=1993top1" scp parser.cpp yxe836@hanlon.wot.eecs.northwestern.edu:compiler/322_framework/L2/src
+sshpass -p "Xy=1993top1" scp L2.h yxe836@hanlon.wot.eecs.northwestern.edu:compiler/322_framework/L2/src
+sshpass -p "Xy=1993top1" scp parser.h yxe836@hanlon.wot.eecs.northwestern.edu:compiler/322_framework/L2/src
